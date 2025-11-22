e82021e77a26189b3e5f92c4ce1304f4085be0a2
Jabbarpatel <jabbarpatel1947@gmail.com>
2025-11-22 16:22:18 +0000

    stiil issues with linux bash files

--- development/backend/entrypoint.sh
+++ development/backend/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -o allexport
-source ../../.env
+source ../.env
 set +o allexport
 
-while ! mysqladmin ping -h "127.0.0.1" -P "$DBPORT" -u "$DBUSERNAME" -p"$DBPASSWORD" --silent; do
-    echo "Waiting for DB on port $DBPORT..."
+while ! mysqladmin ping -h "127.0.0.1" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
+    echo "Waiting for DB on port $MYSQL_PORT..."
     sleep 2
 done
 
